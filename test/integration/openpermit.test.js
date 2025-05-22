24f029a84dac0441d63a6c8d0608ffa8c701c7b2
builtbycorelot <j@corelot.net>
2025-05-22 01:50:19 -0400

    Update test/integration/openpermit.test.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- test/integration/openpermit.test.js
+++ test/integration/openpermit.test.js
@@ -5,14 +5,24 @@ const CompatWorker = require('../../helpers/compat-worker');
 global.Worker = CompatWorker;
 
 describe('OpenPermit with worker', () => {
+  let client;
+
+  beforeEach(() => {
+    client = new OpenPermit({ workerPath: path.join(__dirname, '../../helpers/worker.js') });
+  });
+
+  afterEach(async () => {
+    if (client && client.worker) {
+      await client.worker.terminate();
+    }
+  });
+
   test('create and validate node', async () => {
-    const client = new OpenPermit({ workerPath: path.join(__dirname, '../../helpers/worker.js') });
     const node = await client.createNode({ id: 'n1', type: 'RequirementNode', attributes: { foo: 'bar' } });
     expect(node['@id']).toBe('n1');
     expect(node.attributes.foo).toBe('bar');
 
     const results = await client.validateNode(node);
     expect(results.valid).toBe(true);
-    await client.worker.terminate();
   });
 });
