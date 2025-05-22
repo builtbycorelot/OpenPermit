473f0ee6b1b9b998511c39d6bf7d35453bfa4101
builtbycorelot <j@corelot.net>
2025-05-22 08:20:43 -0400

    Add ESLint flat config

---
+++ eslint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    ignores: ['dist/**', 'node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'commonjs',
+    },
+    rules: {
+    },
+  },
+];
